64597683aa6469eec7c4ecff4c98f3168f29dc0b
Mohamed boussouir <mehamdwsw@gmail.com>
2025-11-18 13:45:20 +0100

    edit

--- indix.js
+++ indix.js
@@ -1,2 +1,18 @@
-const form =document.getElementById("form_Modal");
-console.log(form);
+let arry_Staff=[];
+const butSavechanges =document.getElementById("btn_Save_changes");
+
+console.log();
+butSavechanges.addEventListener('click',add);
+
+function add(){
+const form_Modal =document.getElementById("form_Modal");
+arry_Staff.name=form_Modal[0].value;
+
+
+
+}
+
+
+
+
+
