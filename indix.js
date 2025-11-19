c01c6be96c3b731c7ff23812928918a3f4fcf10f
Mohamed boussouir <mehamdwsw@gmail.com>
2025-11-19 12:28:03 +0100

    edit function add qnd add_2

--- indix.js
+++ indix.js
@@ -1,18 +1,66 @@
-let arry_Staff=[];
-const butSavechanges =document.getElementById("btn_Save_changes");
+let arry_Staff = {
+  Rôle: "",
+  image: "",
+  Email: "",
+  Téléphone: "",
+  Expériences: [],
+};
+const form_Modal = document.getElementById("form_Modal");
+const butSavechanges = document.getElementById("btn_Save_changes");
+const regexName = /^[a-zA-Z\s'-]+$/;
+butSavechanges.addEventListener("click", add);
+const ajouter = document.getElementById("ajouter");
+let nan = false;
 
-console.log();
-butSavechanges.addEventListener('click',add);
+function add() {
+  //   if (!regexName.test(form_Modal[0].value)) {
+  //     return;
+  //   }
+  
+  // const modalElement = document.getElementById('exampleModal');
+  // const modalInstance = bootstrap.Modal.getInstance(modalElement);
+  // modalInstance.hide();
+  nan=true
+  if (nan) {
+    add_2(nan);
+  }
+butSavechanges.setAttribute("data-bs-dismiss", "modal");
 
-function add(){
-const form_Modal =document.getElementById("form_Modal");
-arry_Staff.name=form_Modal[0].value;
+  butSavechanges.click();
+nan=false
+  butSavechanges.setAttribute("data-bs-dismiss", "1");
+
+
 
 
 
 }
-
-
-
-
-
+function add_2() {
+  const profile = document.getElementById("profile");
+  arry_Staff.name = form_Modal[0].value;
+  arry_Staff.Rôle = form_Modal[1].value;
+  arry_Staff.image = form_Modal[2].value;
+  arry_Staff.Email = form_Modal[3].value;
+  arry_Staff.Téléphone = form_Modal[4].value;
+  arry_Staff.Expériences = form_Modal[6].value;
+  profile.innerHTML += `
+ <div class="card profile-card p-3" id="profile">
+                    <div class="d-flex justify-content-center" style="width: 14vh; height: auto;">
+                        <div class=" justify-content-center">
+                            <img src="${arry_Staff.image}"
+                                class=" profile-image rounded-circle" style="width: 5vh; height: 5vh;">
+                            <h6 class="name">${arry_Staff.name}</h6>
+                            <button type="button" class="btn btn-secondary" data-bs-toggle="." data-bs-target=".">
+                                details
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                `;
+  form_Modal[0].value = "";
+  form_Modal[1].value = "";
+  form_Modal[2].value = "";
+  form_Modal[3].value = "";
+  form_Modal[4].value = "";
+  form_Modal[6].value = "";
+}
