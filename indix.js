2fd0879cdc3642b972c86dbbb7b6ebeb7a4e2a7a
Mohamed boussouir <mehamdwsw@gmail.com>
2025-11-19 15:01:35 +0100

    blok

--- indix.js
+++ indix.js
@@ -8,34 +8,27 @@ let arry_Staff = {
 const form_Modal = document.getElementById("form_Modal");
 const butSavechanges = document.getElementById("btn_Save_changes");
 const regexName = /^[a-zA-Z\s'-]+$/;
-butSavechanges.addEventListener("click", add);
+butSavechanges.addEventListener("click", true_Tajoute);
 const ajouter = document.getElementById("ajouter");
 let nan = false;
-
-function add() {
-  //   if (!regexName.test(form_Modal[0].value)) {
-  //     return;
-  //   }
-  
-  // const modalElement = document.getElementById('exampleModal');
-  // const modalInstance = bootstrap.Modal.getInstance(modalElement);
-  // modalInstance.hide();
-  nan=true
-  if (nan) {
-    add_2(nan);
-  }
 butSavechanges.setAttribute("data-bs-dismiss", "modal");
+function true_Tajoute() {
+  
+  
+  
 
+  
+  
   butSavechanges.click();
-nan=false
+
+if (butSavechanges.attributes[3].value="1") {
+    ajouterInDiv();
+    console.log(10);
+    butSavechanges.setAttribute("data-bs-dismiss", "modal");
+  }
   butSavechanges.setAttribute("data-bs-dismiss", "1");
-
-
-
-
-
 }
-function add_2() {
+function ajouterInDiv() { 
   const profile = document.getElementById("profile");
   arry_Staff.name = form_Modal[0].value;
   arry_Staff.Rôle = form_Modal[1].value;
@@ -58,9 +51,15 @@ function add_2() {
                 </div>
                 `;
   form_Modal[0].value = "";
-  form_Modal[1].value = "";
+  form_Modal[1].value = "réception";
   form_Modal[2].value = "";
   form_Modal[3].value = "";
   form_Modal[4].value = "";
   form_Modal[6].value = "";
 }
+// const modalElement = document.getElementById('exampleModal');
+  // const modalInstance = bootstrap.Modal.getInstance(modalElement);
+  // modalInstance.hide();
+  //   if (!regexName.test(form_Modal[0].value)) {
+  //     return;
+  //   }
