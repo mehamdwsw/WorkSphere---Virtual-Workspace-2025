4f7226928ac449adc00450a33d711c529621d338
Mohamed boussouir <mehamdwsw@gmail.com>
2025-11-20 11:47:15 +0100

    edit formvalidation()

--- indix.js
+++ indix.js
@@ -1,10 +1,13 @@
 let arry_Staff = {
-  Rôle: "",
+  Rele: "",
   image: "",
   Email: "",
-  Téléphone: "",
-  Expériences: [],
+  Telephone: "",
+  Experiences: [],
 };
+
+const form_Modal = document.getElementById("form_Modal");
+
 
 // <!-- regex -->
 const regexName = /^[a-zA-Z\s'-]+$/;
@@ -24,7 +27,7 @@ document.getElementById("validation_name").addEventListener("input", () => {
     form_Modal[0].style.border = "thick solid #ee0b0bff";
   }
 });
-document.getElementById("validation_Rôle").addEventListener("click", () => {
+document.getElementById("validation_Role").addEventListener("click", () => {
   if (form_Modal[1].value != "") {
     form_Modal[1].style.border = "thick solid #0ee90aff";
   } else {
@@ -52,7 +55,7 @@ document.getElementById("validation_tel").addEventListener("input", () => {
     form_Modal[4].style.border = "thick solid #ee0b0bff";
   }
 });
-document.getElementById("Expériences").addEventListener("input", () => {
+document.getElementById("Experiences").addEventListener("input", () => {
   if (regexName.test(form_Modal[6].value)) {
     form_Modal[6].style.border = "thick solid #0ee90aff";
   } else {
@@ -63,66 +66,96 @@ document.getElementById("Expériences").addEventListener("input", () => {
 function ajouterInStaff() {
   const profile = document.getElementById("profile");
   arry_Staff.name = form_Modal[0].value;
-  arry_Staff.Rôle = form_Modal[1].value;
+  arry_Staff.Role = form_Modal[1].value;
   arry_Staff.image = form_Modal[2].value;
   arry_Staff.Email = form_Modal[3].value;
-  arry_Staff.Téléphone = form_Modal[4].value;
-  arry_Staff.Expériences = form_Modal[6].value;
+  arry_Staff.Telephone = form_Modal[4].value;
+  arry_Staff.Experiences = form_Modal[6].value;
   profile.innerHTML += `
-  <div class="card profile-card p-3" id="profile">
-  <div class="d-flex justify-content-center" style="width: 14vh; height: auto;">
-  <div class=" justify-content-center">
-  <img src="${arry_Staff.image}"
-  class=" profile-image rounded-circle" style="width: 5vh; height: 5vh;">
-  <h6 class="name">${arry_Staff.name}</h6>
-  <button type="button" class="btn btn-secondary" data-bs-toggle="." data-bs-target=".">
-  details
-  </button>
-  </div>
-  </div>
-  </div>
-  `;
-  form_Modal[0].value = "";
-  form_Modal[1].value = "";
-  form_Modal[2].value = "";
-  form_Modal[3].value = "";
-  form_Modal[4].value = "";
-  form_Modal[6].value = "";
+                        <div class="card profile-card p-3" id="profile">
+                        <div class="d-flex justify-content-center" style="width: 14vh; height: auto;">
+                        <div class=" justify-content-center">
+                        <img src="${arry_Staff.image}"
+                        class=" profile-image rounded-circle" style="width: 5vh; height: 5vh;">
+                        <h6 class="name">${arry_Staff.name}</h6>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="." data-bs-target=".">
+                        details
+                        </button>
+                        </div>
+                        </div>
+                        </div>
+                        `;
+  
 }
 
 function formvalidation() {
   // value of all inputs
 
   let name = form_Modal[0].value;
-  let Rôle = form_Modal[1].value;
+  let Role = form_Modal[1].value;
   let image = form_Modal[2].value;
   let Email = form_Modal[3].value;
-  let Téléphone = form_Modal[4].value;
-  let Expériences = form_Modal[6].value;
+  let Telephone = form_Modal[4].value;
+  let Experiences = form_Modal[6].value;
 
-  const tele_error_zone = document.getElementById("tele-error-zone");
+  const Nom_error_zone = document.getElementById("Nom-error-zone");
+  const Role_error_zone = document.getElementById("Role-error-zone");
+  const Email_error_zone = document.getElementById("Email-error-zone");
+  const Telephone_error_zone = document.getElementById("Telephone-error-zone");
+  const Experiences_error_zone = document.getElementById("Experiences-error-zone");
+  const image_error_zone = document.getElementById("image-error-zone");
   // validation
-  let haserror = true;
+  let haserror = false;
+  // ---------
   if (!regexName.test(name)) {
-    haserror = false;
-  }
-  if (!Rôle) {
-    haserror = false;
-  }
-  if (!imageUrlRegex.test(image)) {
-    haserror = false;
-  }
-  if (!emailRegex.test(Email)) {
-    haserror = false;
-  }
-  if (!phoneNumberRegex.test(Téléphone)) {
-    haserror = false;
-    tele_error_zone.textContent = "*";
+    haserror = true;
+    Nom_error_zone.textContent = "Le nom est incorrect.";
+
   } else {
-    tele_error_zone.textContent = "";
+    Nom_error_zone.textContent ='';
+     haserror = false;
   }
-  if (!regexName.test(Expériences)) {
-    haserror = false;
+  // ---------
+  if (!Role) {
+    haserror = true;
+    Role_error_zone.textContent = "Choisissez une option.";
+  }else {
+    Role_error_zone.textContent = "";
+     haserror = false;
+  }
+  // ---------
+  if (!imageUrlRegex.test(image)) {
+    console.log(image);
+    haserror = true;
+    image_error_zone.textContent = "Le lien vers l'image est incorrect.";
+  }else {
+    image_error_zone.textContent = "";
+     haserror = false;
+  }
+  // ---------
+  if (!emailRegex.test(Email)) {
+    haserror = true;
+    Email_error_zone.textContent = "Adresse e-mail invalide";
+
+  }else {
+    Email_error_zone.textContent = "";
+     haserror = false;
+  }
+  // ---------
+  if (!phoneNumberRegex.test(Telephone)) {
+    haserror = true;
+    Telephone_error_zone.textContent = "Adresse e-mail invalide";
+  } else {
+    Telephone_error_zone.textContent = "";
+     haserror = false;
+  }
+  // ---------
+  if (!regexName.test(Experiences)) {
+    haserror = true;
+    Experiences_error_zone.textContent = "Expériences incorrectes";
+  }else {
+    Experiences_error_zone.textContent = "";
+     haserror = false;
   }
 
   // return true if all the fileds correct, false if not
@@ -131,18 +164,23 @@ function formvalidation() {
 }
 
 // #####################
-const form_Modal = document.getElementById("form_Modal");
-const myModalEl = document.getElementById('exampleModal');
+let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')) // Returns a Bootstrap modal instance
+
 
 const butSavechanges = document.getElementById("btn_Save_changes");
-butSavechanges.addEventListener("click", (e) => {
-  const modalInstance = bootstrap.Modal.getInstance(myModalEl);
-  
-  
-  if (formvalidation()) {
+const button_Experiences = document.getElementById("button_Experiences");
+butSavechanges.addEventListener("click", () => {
+  console.log(formvalidation());
+  if (!formvalidation()) {
     ajouterInStaff();
-    modalInstance.hide();
+    modal.hide();
     console.log(10);
-    
   }
 });
+
+button_Experiences.addEventListener('click',button_Experiences_dynamique)
+function button_Experiences_dynamique(){
+console.log(10);
+
+}
+
